5b13e41c3a5ecf55d28e5570aeea29e90f84328c
Daniel Tran <96daniel.tran@gmail.com>
2018-03-03 11:08:21 -0800

    Edited code for leapmotion

--- leapmotion/main.js
+++ leapmotion/main.js
@@ -2,24 +2,26 @@ var leapjs      = require('leapjs');
 var controller  = new leapjs.Controller({enableGestures: true});
 
 controller.on('deviceFrame', function(frame) {
-  var numberOfFingers = 0;
 
+  var numberOfFingers = 0;
   var hand = frame.hands;
-  for(var h = 0; h < frame.hands.length; h++) {
+
+  for (var h = 0; h < frame.hands.length; h++) {
     console.log("Number of hands: " + frame.hands.length);
     var hand = frame.hands[h];
+    var yNormalOfHand = hand.palmNormal[1];
+    console.log("Palm normal" + yNormalOfHand);
     if (hand.pinchStrength >= 0.89) {
       console.log("Pinch: " + hand.pinchStrength);
     }
-
     for (var f = 0; f < hand.fingers.length; f++){
       console.log(hand.fingers[f] + ' ' + hand.fingers[f].extended);
       numberOfFingers += hand.fingers[f].extended ? 1 : 0;
     }
-    console.log("Fingers: " + numberOfFingers);
+
 
   }
-
+  console.log("Fingers: " + numberOfFingers);
 });
 
 controller.connect();
