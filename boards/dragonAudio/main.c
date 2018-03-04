7fd5decb4eb0c1a797bf36a30c0645d35993066f
sjfricke <spencerfricke@gmail.com>
2018-03-04 02:42:22 +0000

    server test works on board

--- boards/dragonAudio/main.c
+++ boards/dragonAudio/main.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "gpio.h"
+#include <poll.h>
+#include <fcntl.h> // open()
+#include <pthread.h>
 
-#define A1 33
-#define A2 34
-#define B1 28
-#define B2 35
+#include "server/server.h"
+#include "modules/gpio.h"
 
-void setStep(int w1, int w2, int x1, int x2) {
-  GpioSetValue(A1, w1);
-  GpioSetValue(A2, w2);
-  GpioSetValue(B1, x1);
-  GpioSetValue(B2, x2);
-}
+extern server_t* g_server;
 
-void forward(int delay, int steps) {
-  int i;
-  for (i = 0; i < steps; i++)  {
-    setStep(1,0,0,1);
-    usleep(delay);
-    setStep(0,1,0,1);
-    usleep(delay);
-    setStep(0,1,1,0);
-    usleep(delay);
-    setStep(1,0,1,0);
-    usleep(delay);
+void socketCallback( int type, const char* value) {
+  // To declare variables inside case you need to enable a scope with { }
+  printf("type: %d\t value: %s\n", type, value);
+  broadcastInt("2", 4);
+  // printf("DEBUG - Data called back of %d : %s\n", type, value);
+  switch(type) {
+
+  case 0:
+      break;
+
+  case 1: {
+    break;
+  }
+  case 2:
+     break;
+
+  default:
+    printf("Not a valid type! [%d]\n", type);
+    break;
   }
 }
 
-int main(int argc, char* argv[]) {
 
-  GpioOutput(A1, 0);
-  GpioOutput(A2, 0);
-  GpioOutput(B1, 0);
-  GpioOutput(B2, 0);
+int main ( int argc, char* argv[] ) {
 
-  forward(atoi(argv[1]), atoi(argv[2]));
+  g_server = (server_t*)malloc(sizeof(server_t));
+  g_server->port = 8000;
+  g_server->onSocketMessage = socketCallback;
+
+  startServer();
+
+  // main infinite loop
+  while(1) {
+
+    usleep(5000000); // 5 sec
+  }
+
 }
