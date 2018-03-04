5d31dd15d7ea6d332b74c277446a79c83ff589ee
sjfricke <sjfricke@wisc.edu>
2018-03-04 05:37:32 -0800

    tcp server started

--- webserver/server.js
+++ webserver/server.js
@@ -6,13 +6,14 @@ const path = require('path'); //Node.js module used for getting path of file
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const app = express();
 const bodyParser = require('body-parser'); //allows the use of req.body in POST request
+const WebSocket = require('ws');
+
 var arr=[];
 
 app.use(express.static(path.join(__dirname, 'public'))); //sets all static file calls to
 
 app.use(bodyParser.json()); //parses json and sets to body
 app.use(bodyParser.urlencoded({ extended: false }));
-var arr[]="";
 app.post('/sms', (req, res) => {
 
   //Write code to send to fidget spinner.
@@ -53,3 +54,88 @@ app.get('/data', (req, res) => {
 http.createServer(app).listen(PORT, () => {
   console.log('Express server listening on port 3000');
 });
+
+
+/////////////////////////////
+/// DRAGON BOARD AUDIO //////
+/////////////////////////////
+const dragonAudio = new WebSocket('ws://192.168.43.151:8000');
+
+dragonAudio.on('open', function open() { 
+	dragonAudio.send('0:0');
+});
+
+dragonAudio.on('message', function incoming(data) {
+  console.log(data);
+});
+
+
+
+/////////////////////////
+////// TCP SERVER ///////
+/////////////////////////
+
+// Load the TCP Library
+const net = require('net');
+
+// Keep track of the chat clients
+var clients = [];
+var completeData;
+// Start a TCP Server
+net.createServer(function (tSocket) {
+
+
+  // Identify this client
+  tSocket.name = tSocket.remoteAddress + ":" + tSocket.remotePort 
+  console.log(tSocket.name + " has joined!");
+
+  // Put this new client in the list
+  clients.push(tSocket);
+
+  // Send a nice welcome message and announce
+ // tSocket.write("Welcome " + tSocket.name + "\n");
+
+  // Handle incoming messages from clients.
+  tSocket.on('data', function (data) {
+  	var read = data.toString();
+	console.log(read);
+  	var key = read.substring(0, read.indexOf(":"));
+  	if (key == 8) {
+  		piData(read.substr(2));
+  	} else if (key == 4) {
+  		myoData(read.substr(2));
+  	} else if (key == 2) {
+  		leapData(read.substr(2));
+  	}
+  });
+
+  // Remove the client from the list when it leaves
+  tSocket.on('end', function () {
+    clients.splice(clients.indexOf(tSocket), 1);
+    console.log(tSocket.name + " left the chat.\n");
+  });
+  
+  // Send a message to all clients
+
+}).listen(5000);
+
+function piData(data) {
+	var sec = data.split(",");
+	//Joy_X, Joy_y, Joy_Btn, Pot, Temp
+}
+
+function myoData(data) {
+	console.log(data);
+}
+
+function leapData(data) {
+	console.log(data);
+}
+
+
+// Put a friendly message on the terminal of the server.
+console.log("Chat server running at port 5000\n");
+
+process.on('uncaughtException',function(err){
+   console.log('something terrible happened..')
+})
--- webserver/socketTest.js
+++ webserver/socketTest.js
@@ -1,11 +1,42 @@
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://192.168.43.151:8000');
+// const ws = new WebSocket('ws://192.168.43.151:8000');
 
-ws.on('open', function open() {
-  ws.send('1:something');
-});
+// ws.on('open', function open() {
+//   ws.send('1:something');
+// });
 
-ws.on('message', function incoming(data) {
-  console.log(data);
-});	
+// ws.on('message', function incoming(data) {
+//   console.log(data);
+// });	
+
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 5000;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
